5abad1056ad50cd7b31403332d6e3c17dde83f4c
AladeenSun <sunzhouyi@gmail.com>
2016-12-22 01:20:50 +0800

    Comments added

--- graph-lib.scm
+++ graph-lib.scm
@@ -3,17 +3,17 @@
 (load "grapheme.scm")
 (load "list-lib.scm")
 
-; Graph operations
+; Fetch some items from G
 
 (define (vertexes G) (car G))
 (define (arcs G) (cadr G))
 (define (vertex G pos) (list-ref (vertexes G) pos))
 (define (arc G pos) (list-ref (arcs G) pos))
 
-
-(define UPPER '(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z))
+;Relabel the vertexes of G by capitals
 (define (relabel G)
-	(define v (vertexes G))
+	(define UPPER '(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z))
+  	(define v (vertexes G))
 	(define a (arcs G))
 	(define v-map (zip v (take (length v) UPPER)))
 	(define (f node) (cadr (assf (curry eqs? node) v-map)))
@@ -28,7 +28,11 @@
 	(define a-new (map a-modify a))
 	(cons v-new (list a-new)))
 
+;The alphabet of G
+
 (define (lex G) 
 	(filter (lambda (x) (not (eq? x '#))) (remove-duplicates (flatten (map (compose atom-split caddr) (arcs G))))))
+
+; Drop the leading # of vertexes
 
 (define (normal-v v) (map (lambda (x) (if (acc? x) (sflip x) x)) v))
