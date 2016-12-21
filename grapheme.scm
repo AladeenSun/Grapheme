c4e6055accdcdcaad5a6750b93b28b8b0bfc72d9
AladeenSun <sunzhouyi@gmail.com>
2016-12-22 01:33:13 +0800

    Comments modified

--- grapheme.scm
+++ grapheme.scm
@@ -1,44 +1,4 @@
-; Syntactic sugars for Grapheme
-
-
-; Syntactic sugar for car and adr
-
-(define (caar pair) (car (car pair)))
-(define (cadr pair) (car (cdr pair)))
-(define (cdar pair) (cdr (car pair)))
-(define (cddr pair) (cdr (cdr pair)))
-(define (caaar pair) (car (car (car pair))))
-(define (caadr pair) (car (car (cdr pair))))
-(define (cadar pair) (car (cdr (car pair))))
-(define (caddr pair) (car (cdr (cdr pair))))
-(define (cdaar pair) (cdr (car (car pair))))
-(define (cdadr pair) (cdr (car (cdr pair))))
-(define (cddar pair) (cdr (cdr (car pair))))
-(define (cdddr pair) (cdr (cdr (cdr pair))))
-(define (caaaar pair) (car (car (car (car pair)))))
-(define (caaadr pair) (car (car (car (cdr pair)))))
-(define (caadar pair) (car (car (cdr (car pair)))))
-(define (caaddr pair) (car (car (cdr (cdr pair)))))
-(define (cadaar pair) (car (cdr (car (car pair)))))
-(define (cadadr pair) (car (cdr (car (cdr pair)))))
-(define (caddar pair) (car (cdr (cdr (car pair)))))
-(define (cadddr pair) (car (cdr (cdr (cdr pair)))))
-(define (cdaaar pair) (cdr (car (car (car pair)))))
-(define (cdaadr pair) (cdr (car (car (cdr pair)))))
-(define (cdadar pair) (cdr (car (cdr (car pair)))))
-(define (cdaddr pair) (cdr (car (cdr (cdr pair)))))
-(define (cddaar pair) (cdr (cdr (car (car pair)))))
-(define (cddadr pair) (cdr (cdr (car (cdr pair)))))
-(define (cdddar pair) (cdr (cdr (cdr (car pair)))))
-(define (cddddr pair) (cdr (cdr (cdr (cdr pair)))))
-
-
-; Conditionals
-
-(define (not x)            (if x #f #t))
-(define (and . lst)         (fold && #t lst))
-(define (or . lst)          (fold || #f lst))
-
+; Some fundamental operations for Grapheme
 
 ; Functional attributes
 
@@ -47,14 +7,8 @@
 (define (compose f g)      (lambda (arg) (f (g arg))))
 
 
-; Pair constructors and selectors
-
-(define (null? obj)        (if (eqv? obj '()) #t #f))
-(define (list . objs)       objs)
-
-
-; Some useful syntactic sugar
-
+; Basic functions
+(define (not x)            (if x #f #t))
 (define (id obj)           obj)
 (define zero?              (curry = 0))
 (define positive?          (curry < 0))
@@ -63,8 +17,6 @@
 (define (odd? num)         (= (mod num 2) 1))
 (define (even? num)        (= (mod num 2) 0))
 (define (abs x)            ((> x 0) x -x))
-(define (max x . num-list) (fold (lambda (y z) (if (> y z) y z)) x num-list))
-(define (min x . num-list) (fold (lambda (y z) (if (< y z) y z)) x num-list))
 
 
 'Done!
